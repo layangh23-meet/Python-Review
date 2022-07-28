a381081b6419a4f7f2537f7e81ba808c56654a5b
layangh23-meet <109951502+layangh23-meet@users.noreply.github.com>
2022-07-28 14:40:14 +0300

    im done with python review

---
+++ pythonReview.py
@@ -0,0 +1,18 @@
+def create_youtube_video (title,description):
+    youtubevideo = {"title":title , "description":description,"likes":100, "dislike":0,"comments":{}}
+
+def like (youtubevideo):  
+	if "likes" in youtubevideo: 
+		youtubevideo ["likes"] += 1 
+		return youtubevideo
+
+def dislike (youtubevideo):
+	if "dislike" in youtubevideo:
+		youtubevideo ["dislike"] += 1
+
+def add_comment(youtubevideo ,username , comment_text):
+	youtubevideo["comments"][username]=comment_text
+	return youtubevideo
+
+	dictonary = create_youtube_video 
+	
